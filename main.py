36cb2525d38fb87c81a8d4187acf79d2a7b67d82
kmutyala01 <karthiksatya@gmail.com>
2022-05-14 17:21:37 -0500

    Finding price implemented

--- main.py
+++ main.py
@@ -6,25 +6,36 @@ import requests
 import time
 
 
-url = "https://www.apartments.com/ave-las-colinas-irving-tx/qfsptny/"
+url = "https://www.apartments.com/two99-monroe-roanoke-tx/xgb8vqm/"
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 response = requests.get(url, headers=headers)
 
 print(response)
 
 soup = BeautifulSoup(response.content,'html.parser')
-
+model_response = soup.find_all('span', class_ = "modelName")
 price_response = soup.find_all('span',class_ = "rentLabel")
-print(len(price_response))
+apartment_info = soup.find_all('h3', class_ = "modelLabel")
 all_prices = []
+#print(price_response)
+model_prices = {}
 
-for price in price_response:
-    curr = price.text.strip()
-    print(curr)
-    #if curr[0] == "$":
-    #     ch = curr[6:10]
-    #     all_prices.extend(curr[1:].split(ch))
+def notify():
+    print("email sent")
+    
 
-    # print(price.text.strip())
-#     print(len(price.text.strip()))
-    # print('*****')
+
+for apartment in apartment_info:
+    curr = apartment.text.split("$")
+
+    modelName = curr[0].split(" ")[0].split('\n')[1]
+    
+    print(modelName)
+    if modelName == "A1":
+        price = curr[1].split("–")[0][:-1]
+        price = price.split(",")
+        price = int(''.join(price[0 : 2]))
+        if price <= 1500:
+            notify()
+
+
